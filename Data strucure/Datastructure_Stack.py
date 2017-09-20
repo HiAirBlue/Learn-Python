b1c514de014af474af07bb198a2b8b433bf878fb
HiAirBlue <my.place@foxmail.com>
2017-09-20 19:58:52 +0800

    十进制到N进制转换

--- Data strucure/Datastructure_Stack.py
+++ Data strucure/Datastructure_Stack.py
@@ -32,11 +32,10 @@ def parChecker(symbolString):
 		if symbolString[i] in "([{)]}":
 			if symbolString[i] in "([{":
 				s.push(symbolString[i])
-				print('t1:',s.peek())
 			else:
 				if s.isEmpty():
 					balance = False
-					print('t2:',balance,s.isEmpty())
+					#print('t2:',balance,s.isEmpty())
 					break
 				else:
 					opens = "([{"
@@ -45,12 +44,30 @@ def parChecker(symbolString):
 					print(top)
 					if not opens.index(top) == closes.index(symbolString[i]):
 						balance = False
-						print('t3',balance)
+						#print('t3',balance)
 						break
 		i += 1
-		print("i=",i)
+		#print("i=",i)
 	if balance and s.isEmpty():
 		return True
 	else:
 		return False
 
+#算法二  十进制到N进制转换
+
+def baseConverter(decNum, base):
+	"""用于十进制向n进制转换"""
+
+	digits = "0123456789ABCDEF"
+	s = Stack()
+
+	while decNum > 0:
+		rem = decNum % base
+		s.push(rem)
+		decNum = decNum // base
+
+	newNum = ""
+	while not s.isEmpty():
+		newNum = newNum + digits[s.pop()]
+	
+	return newNum
